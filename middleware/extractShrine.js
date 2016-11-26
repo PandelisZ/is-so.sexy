dc57a75ebcc17d41525a988b2d2bc14879d50a09
Matt Burman <matt@mattburman.com>
2016-11-26 23:43:42 +0000

    Single shrine pull

--- middleware/extractShrine.js
+++ middleware/extractShrine.js
@@ -3,12 +3,12 @@ const Shrine = require('../models/Shrine');
 module.exports = (req, res, next) => {
 	const firstSub = req.subdomains[0];
 
-	return Shrine.find({ subdomain: firstSub }, (err, docs) => {
+	return Shrine.findOne({ subdomain: firstSub }, (err, doc) => {
 		if (err) return res.status(503).send("DB down or something lol");
 
-		if (docs.length === 0) return res.redirect('/manage');
+		if (!doc) return res.redirect('/manage');
 
-		req.shrines = docs;
+		req.shrine = doc;
 
 		return next();
 	});
