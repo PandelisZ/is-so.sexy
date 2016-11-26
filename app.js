8f9613e9bfa8204b238304340e907f72f718b53b
Matt Burman <matt@mattburman.com>
2016-11-26 23:03:46 +0000

    Fix path in middleware require

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const multer = require('multer');
 
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
-const extractShrine = require('../middleware/extractShrine');
+const extractShrine = require('./middleware/extractShrine');
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
