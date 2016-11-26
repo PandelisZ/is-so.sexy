9348ba75d0441b4cf1e965063ebafa35574c14a8
Matt Burman <matt@mattburman.com>
2016-11-26 18:58:59 +0000

    Try root router: rooter

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const expressValidator = require('express-validator');
 const expressStatusMonitor = require('express-status-monitor');
 const sass = require('node-sass-middleware');
 const multer = require('multer');
+const subdomain = require('express-subdomain');
 
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
@@ -45,6 +46,12 @@ const passportConfig = require('./config/passport');
  * Create Express server.
  */
 const app = express();
+
+/**
+ * Define some routers
+ */
+const rooter = express.Router();
+const subrouter = express.Router();
 
 /**
  * Connect to MongoDB.
@@ -116,105 +123,105 @@ app.use(express.static(path.join(__dirname, 'public'), { /*maxAge: 31557600000*/
 /**
  * Primary app routes.
  */
-app.get('/', homeController.index);
-app.get('/login', userController.getLogin);
-app.post('/login', userController.postLogin);
-app.get('/logout', userController.logout);
-app.get('/forgot', userController.getForgot);
-app.post('/forgot', userController.postForgot);
-app.get('/reset/:token', userController.getReset);
-app.post('/reset/:token', userController.postReset);
-app.get('/signup', userController.getSignup);
-app.post('/signup', userController.postSignup);
-app.get('/contact', contactController.getContact);
-app.post('/contact', contactController.postContact);
-app.get('/account', passportConfig.isAuthenticated, userController.getAccount);
-app.get('/generator', generatorController.newShrine);
-app.post('/generator/create', generatorController.createShrine)
-//app.post('/generator/create', generatorController.createShrine);
-app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
-app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
-app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
-app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
+rooter.get('/', homeController.index);
+rooter.get('/login', userController.getLogin);
+rooter.post('/login', userController.postLogin);
+rooter.get('/logout', userController.logout);
+rooter.get('/forgot', userController.getForgot);
+rooter.post('/forgot', userController.postForgot);
+rooter.get('/reset/:token', userController.getReset);
+rooter.post('/reset/:token', userController.postReset);
+rooter.get('/signup', userController.getSignup);
+rooter.post('/signup', userController.postSignup);
+rooter.get('/contact', contactController.getContact);
+rooter.post('/contact', contactController.postContact);
+rooter.get('/account', passportConfig.isAuthenticated, userController.getAccount);
+rooter.get('/generator', generatorController.newShrine);
+rooter.post('/generator/create', generatorController.createShrine)
+//rooter.post('/generator/create', generatorController.createShrine);
+rooter.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
+rooter.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
+rooter.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
+rooter.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
 
 /**
  * API examples routes.
  */
-app.get('/api', apiController.getApi);
-app.get('/api/lastfm', apiController.getLastfm);
-app.get('/api/nyt', apiController.getNewYorkTimes);
-app.get('/api/aviary', apiController.getAviary);
-app.get('/api/steam', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getSteam);
-app.get('/api/stripe', apiController.getStripe);
-app.post('/api/stripe', apiController.postStripe);
-app.get('/api/scraping', apiController.getScraping);
-app.get('/api/twilio', apiController.getTwilio);
-app.post('/api/twilio', apiController.postTwilio);
-app.get('/api/clockwork', apiController.getClockwork);
-app.post('/api/clockwork', apiController.postClockwork);
-app.get('/api/foursquare', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFoursquare);
-app.get('/api/tumblr', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getTumblr);
-app.get('/api/facebook', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
-app.get('/api/github', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getGithub);
-app.get('/api/twitter', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getTwitter);
-app.post('/api/twitter', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.postTwitter);
-app.get('/api/linkedin', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getLinkedin);
-app.get('/api/instagram', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getInstagram);
-app.get('/api/paypal', apiController.getPayPal);
-app.get('/api/paypal/success', apiController.getPayPalSuccess);
-app.get('/api/paypal/cancel', apiController.getPayPalCancel);
-app.get('/api/lob', apiController.getLob);
-app.get('/api/upload', apiController.getFileUpload);
-app.post('/api/upload', upload.single('myFile'), apiController.postFileUpload);
-app.get('/api/pinterest', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getPinterest);
-app.post('/api/pinterest', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.postPinterest);
-app.get('/api/google-maps', apiController.getGoogleMaps);
+rooter.get('/api', apiController.getApi);
+rooter.get('/api/lastfm', apiController.getLastfm);
+rooter.get('/api/nyt', apiController.getNewYorkTimes);
+rooter.get('/api/aviary', apiController.getAviary);
+rooter.get('/api/steam', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getSteam);
+rooter.get('/api/stripe', apiController.getStripe);
+rooter.post('/api/stripe', apiController.postStripe);
+rooter.get('/api/scraping', apiController.getScraping);
+rooter.get('/api/twilio', apiController.getTwilio);
+rooter.post('/api/twilio', apiController.postTwilio);
+rooter.get('/api/clockwork', apiController.getClockwork);
+rooter.post('/api/clockwork', apiController.postClockwork);
+rooter.get('/api/foursquare', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFoursquare);
+rooter.get('/api/tumblr', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getTumblr);
+rooter.get('/api/facebook', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
+rooter.get('/api/github', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getGithub);
+rooter.get('/api/twitter', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getTwitter);
+rooter.post('/api/twitter', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.postTwitter);
+rooter.get('/api/linkedin', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getLinkedin);
+rooter.get('/api/instagram', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getInstagram);
+rooter.get('/api/paypal', apiController.getPayPal);
+rooter.get('/api/paypal/success', apiController.getPayPalSuccess);
+rooter.get('/api/paypal/cancel', apiController.getPayPalCancel);
+rooter.get('/api/lob', apiController.getLob);
+rooter.get('/api/upload', apiController.getFileUpload);
+rooter.post('/api/upload', upload.single('myFile'), apiController.postFileUpload);
+rooter.get('/api/pinterest', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getPinterest);
+rooter.post('/api/pinterest', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.postPinterest);
+rooter.get('/api/google-maps', apiController.getGoogleMaps);
 
 /**
  * OAuth authentication routes. (Sign in)
  */
-app.get('/auth/instagram', passport.authenticate('instagram'));
-app.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/login' }), (req, res) => {
+rooter.get('/auth/instagram', passport.authenticate('instagram'));
+rooter.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/login' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
+rooter.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
+rooter.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
-app.get('/auth/github', passport.authenticate('github'));
-app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), (req, res) => {
+rooter.get('/auth/github', passport.authenticate('github'));
+rooter.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
-app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+rooter.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
+rooter.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
-app.get('/auth/twitter', passport.authenticate('twitter'));
-app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), (req, res) => {
+rooter.get('/auth/twitter', passport.authenticate('twitter'));
+rooter.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
-app.get('/auth/linkedin', passport.authenticate('linkedin', { state: 'SOME STATE' }));
-app.get('/auth/linkedin/callback', passport.authenticate('linkedin', { failureRedirect: '/login' }), (req, res) => {
+rooter.get('/auth/linkedin', passport.authenticate('linkedin', { state: 'SOME STATE' }));
+rooter.get('/auth/linkedin/callback', passport.authenticate('linkedin', { failureRedirect: '/login' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
 
 /**
  * OAuth authorization routes. (API examples)
  */
-app.get('/auth/foursquare', passport.authorize('foursquare'));
-app.get('/auth/foursquare/callback', passport.authorize('foursquare', { failureRedirect: '/api' }), (req, res) => {
+rooter.get('/auth/foursquare', passport.authorize('foursquare'));
+rooter.get('/auth/foursquare/callback', passport.authorize('foursquare', { failureRedirect: '/api' }), (req, res) => {
   res.redirect('/api/foursquare');
 });
-app.get('/auth/tumblr', passport.authorize('tumblr'));
-app.get('/auth/tumblr/callback', passport.authorize('tumblr', { failureRedirect: '/api' }), (req, res) => {
+rooter.get('/auth/tumblr', passport.authorize('tumblr'));
+rooter.get('/auth/tumblr/callback', passport.authorize('tumblr', { failureRedirect: '/api' }), (req, res) => {
   res.redirect('/api/tumblr');
 });
-app.get('/auth/steam', passport.authorize('openid', { state: 'SOME STATE' }));
-app.get('/auth/steam/callback', passport.authorize('openid', { failureRedirect: '/login' }), (req, res) => {
+rooter.get('/auth/steam', passport.authorize('openid', { state: 'SOME STATE' }));
+rooter.get('/auth/steam/callback', passport.authorize('openid', { failureRedirect: '/login' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
-app.get('/auth/pinterest', passport.authorize('pinterest', { scope: 'read_public write_public' }));
-app.get('/auth/pinterest/callback', passport.authorize('pinterest', { failureRedirect: '/login' }), (req, res) => {
+rooter.get('/auth/pinterest', passport.authorize('pinterest', { scope: 'read_public write_public' }));
+rooter.get('/auth/pinterest/callback', passport.authorize('pinterest', { failureRedirect: '/login' }), (req, res) => {
   res.redirect('/api/pinterest');
 });
 
