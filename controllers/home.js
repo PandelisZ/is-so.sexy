5844e53844d01363130f3b34653552695f6d9476
Pandelis Zembashis <info@pandelis.me>
2016-11-27 12:20:52 +0000

    added single music file

--- controllers/home.js
+++ controllers/home.js
@@ -11,10 +11,16 @@ exports.index = (req, res) => {
 		let uploads = shrine.images.map((img)=>{
 			return '/uploads/'+img
 		})
+		var music = '/music/audio2.mp3'
+		if (shrine.music){
+			music = '/music/' + shrine.music
+		}
+
 		res.render('shrine/default', {
 			title: shrine.name + ' is so sexy!',
 			description: shrine.description,
-			images: JSON.stringify(uploads)
+			images: JSON.stringify(uploads),
+			music: music
 		})
 	}else {
 		res.render('home', {
