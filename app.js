a090dad0c9f2cd895a19bf71b2fdebb1204445ec
Pandelis Zembashis <info@pandelis.me>
2016-11-27 11:35:32 +0000

    working image slideshows

--- app.js
+++ app.js
@@ -114,6 +114,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.static(path.join(__dirname, 'public'), { /*maxAge: 31557600000*/ }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 /**
  * Primary app routes.
--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,25 @@
+
 /**
  * GET /
  * Home page.
  */
 exports.index = (req, res) => {
 	console.log(req.shrine);
-  res.render('home', {
-    title: 'Home'
-  });
+
+	if(req.shrine){
+		let shrine = req.shrine
+		let uploads = shrine.images.map((img)=>{
+			return '/uploads/'+img
+		})
+		res.render('shrine/default', {
+			title: shrine.name + ' is so sexy!',
+			description: shrine.description,
+			images: JSON.stringify(uploads)
+		})
+	}else {
+		res.render('home', {
+	    title: 'Home'
+	  });
+	}
+
 };
