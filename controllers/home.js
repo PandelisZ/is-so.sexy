9754b8326b14e3b43abf081d479537a1522c12cb
Daniel Carl Jones <daniel.carl@live.com>
2016-11-27 09:20:05 +0000

    Setup controller

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,30 @@
+const async = require('async');
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+const passport = require('passport');
+const User = require('../models/User');
+const Shrine = require('../models/Shrine');
+
 /**
  * GET /
- * Home page.
+ * Main shrine page.
  */
-exports.index = (req, res) => {
-  res.render('home', {
-    title: 'Home'
-  });
+exports.getShrine = (req, res) => {
+  // Assume single shrine is in req
+  res.send("BANTER");
+  banter = {
+  	"name" : "Matt",
+  	"description" : "test",
+  	"subdomain" : "test",
+  	"owner" : "583a1483fdbef6203317b176",
+  	"images" : [
+  		"c0512597c7b392dc67b887b8825dd389",
+  		"cfe2a3a40070d7d9978785c6c89af321"
+    ]
+  };
+  console.log(banter);
+  // res.render('home', {
+  //   // ensure home is setup to display stuff
+  //   title: 'Login'
+  // });
 };
